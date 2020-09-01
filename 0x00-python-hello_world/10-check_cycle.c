a1652a896f5f57e9f2480d7055be096e9b6c6af4
Camilo Barreiro <1845@holbertonschool.com>
2020-09-01 02:00:54 +0000

    Exercise 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * check_cycle - finds loops in a linked list
+ * @list: pointerto the head of the list
+ * Return: 1 on loop 0 whrn no loop found
+ */
+
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
