f3434125f3a60c85a801b65b261f2e642bcd86f9
Camilo Barreiro <1845@holbertonschool.com>
2020-09-03 21:17:46 +0000

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     Number = len(sys.argv)
-    if(Number <= 1):
-        print("0 arguments.")
+    if Number <= 1:
+        print("0 argument.")
     else:
         if Number == 2:
-            print("{:d} argument:".format(Number-1))
+            print("{:d} argument:".format(Number - 1))
         else:
-            print("{:d} argument:".format(Number-1))
-            for i in range(1, Number):
-                print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d} arguments:".format(Number - 1))
+        for i in range(1, Number):
+            print("{:d}: {}".format(i, sys.argv[i]))
