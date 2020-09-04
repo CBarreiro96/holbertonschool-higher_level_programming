ee2f94832652a5cc73984c015b04399926096434
Camilo Barreiro <1845@holbertonschool.com>
2020-09-04 02:03:32 +0000

    Correction requirement fail

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     Result = 0
     Number = len(argv)
-    for i in range(1 , Number):
+    for i in range(1, Number):
         Result += argv[i]
     print("{:d}".format(Result))
