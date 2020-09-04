2aef60bb75ef7f6c89c7a5938f7f0dbd18d7316a
Camilo Barreiro <1845@holbertonschool.com>
2020-09-04 02:44:26 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    Dir_to_show = dir(hidden_4)
+    Symbol_to_avoid = "__"
+    for i in range(0, len(Dir_to_show)):
+        if Symbol_to_avoid not in Dir_to_show[i]:
+            print(Dir_to_show[i])
