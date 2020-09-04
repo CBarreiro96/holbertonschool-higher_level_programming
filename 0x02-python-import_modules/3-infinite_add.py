6510aa9ea74d6e79ad2d48145ae9fa6eb2878d81
Camilo Barreiro <1845@holbertonschool.com>
2020-09-04 02:01:44 +0000

    Exercise infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    Result = 0
+    Number = len(argv)
+    for i in range(1 , Number):
+        Result += argv[i]
+    print("{:d}".format(Result))
