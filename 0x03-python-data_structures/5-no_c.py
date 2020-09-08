3676cc8258499398ba2b44cf7814c4382c95be6f
Camilo Barreiro <1845@holbertonschool.com>
2020-09-08 21:45:17 +0000

    exercise 5 no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new_string += i
+    return(new_string)
+#return my_string.translate({ord(c) : None for c in "cC"})(alternative)
