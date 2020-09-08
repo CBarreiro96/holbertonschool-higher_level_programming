61992000e08679c3a4b950ad7893333d1745f118
Camilo Barreiro <1845@holbertonschool.com>
2020-09-08 20:16:50 +0000

    neww list in python

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    my_list2 = my_list[:]
+    if (0 <= idx < len(my_list)):
+        my_list2[idx] = element
+    else:
+        return(my_list)
+    return(my_list2)
