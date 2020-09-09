c0a528f045c51a7f82c72c3eae999ef9f512ff97
Camilo Barreiro <1845@holbertonschool.com>
2020-09-09 02:28:32 +0000

    add file 10-divisible and 9-max integer

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
+    my_list2 = []
+    for n in my_list:
+        if n % 2 == 0:
+            my_list2.append(True)
+        else:
+            my_list2.append(False)
+    return my_list2
