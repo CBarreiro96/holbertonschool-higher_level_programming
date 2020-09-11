a9732e1451203b27106ef2c84ce38db9d9f0460a
Camilo Barreiro <1845@holbertonschool.com>
2020-09-11 02:11:39 +0000

    add exercise roma

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    new_rom_value = 0
+    for i in range(len(roman_string)):
+        if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i-1]]:
+            new_rom_value += rom_val[roman_string[i]]-2
+        else:
+            new_rom_value += rom_val[roman_string[i]]
+    return new_rom_value
