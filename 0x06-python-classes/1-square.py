61f10a15ee6b25b83913266436d446380b79dd50
Camilo Barreiro <1845@holbertonschool.com>
2020-09-22 22:25:00 +0000

    Correction fail request

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Class square"""
+"""
+Class square
+"""
 class Square:
-    """A class that defines a Square by size"""
+"""A class that defines a Square by size"""
     def __init__(self, size):
         self.__size = size
