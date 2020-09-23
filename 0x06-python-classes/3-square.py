e57ea20d0303e0126fab6aa8aa8faa5f62b4db5e
Camilo Barreiro <1845@holbertonschool.com>
2020-09-23 00:18:40 +0000

    Square and area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Class square"""
+
+
+class Square:
+    """Class define a square by size and It can compute an area"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
