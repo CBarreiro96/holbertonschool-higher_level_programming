a0a35eb6129fc8e987ebcdd24b2e046f5137aa3c
Camilo Barreiro <1845@holbertonschool.com>
2020-10-02 15:55:08 +0000

    pep validation exercise text identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,10 @@ This is text indentation module
 
 
 def text_indentation(text):
-    """Print a text with 2 new lines after each of these characters: ., ? and :"""
+    """
+    Print a text with 2 new lines after each
+    of these characters: ., ? and :
+    """
     if text is None or not isinstance(text, str) or len(text) < 0:
         raise TypeError("text must be a string")
     n = "".join([c if c not in "?.:" else c + "\n\n" for c in text])
