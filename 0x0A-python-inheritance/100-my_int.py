26c7f5e59271becae7e184649df6243eb82787fc
Camilo Barreiro <1845@holbertonschool.com>
2020-10-07 01:46:59 +0000

    task advance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/python3
+"""Module for MyInt class."""
+
+
+class MyInt(int):
+    """MyInt class."""
+    def __eq__(self, other):
+        """If are equals, inverting it."""
+        return self - other != 0
+
+    def __ne__(self, other):
+        """If are not-equals, inverting it."""
+        return self - other == 0
