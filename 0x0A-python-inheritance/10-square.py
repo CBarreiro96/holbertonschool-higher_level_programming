26d9bea6a9a50ffec0584552ea3f4c2b651917cc
Camilo Barreiro <1845@holbertonschool.com>
2020-10-07 01:39:41 +0000

    add method area

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,4 +6,11 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """ Class that defines a Square by inheritance of Rectangle class """
     def __init__(self, size):
+        """ Constructor """
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        """ Area method through Super call """
+        return super().area()
