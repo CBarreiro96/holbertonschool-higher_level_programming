1c329eb252d646453ac5a634c112eaa6dbaddc2c
Camilo Barreiro <1845@holbertonschool.com>
2020-10-07 01:01:13 +0000

    correction fail request

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Geometry Class """
 class BaseGeometry:
+    """ Class that implemented area """
     def area(self):
+        """ Not Yet Implemented """
         raise Exception("area() is not implemented")
