de263bcb357925104ff07287657837f596c9b447
Camilo Barreiro <1845@holbertonschool.com>
2020-10-07 22:21:07 +0000

    correction requeriment file

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+returns an object (Python data structure)
+represented by a JSON string
+"""
 import json
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """function that returns an object
+    (Python data structure) represented by a JSON string"""
 
     return json.loads(my_str)
