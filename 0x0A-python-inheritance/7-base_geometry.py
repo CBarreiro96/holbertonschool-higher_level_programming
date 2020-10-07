d8b9f3fc9143f26cb61a219d3219014edfe0c9f3
Camilo Barreiro <1845@holbertonschool.com>
2020-10-07 00:59:09 +0000

    Correction fail request

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,8 +1,16 @@
 #!/bin/usr/python3
+"""Geometry Class"""
+
+
 class BaseGeometry:
+    """Class that define area and integer validator"""
+
     def area(self):
+        """Exception that area is not implemented"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
+        """Integer validator"""
         if(type(value) != int):
             raise TypeError("{} must be an integer".format(name))
         elif(value <= 0):
