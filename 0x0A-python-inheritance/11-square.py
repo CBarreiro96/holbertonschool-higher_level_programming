41bc6b517c441585c23bfa2b38719e23166e0a7c
Camilo Barreiro <1845@holbertonschool.com>
2020-10-07 00:50:38 +0000

    correction gile riquest

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+
+""" Class that inherits from Rectangle """
+
 Rectangle =__import__("9-rectangle").Rectangle
+
 
 class Square(Rectangle):
     def __init__(self, size):
+        """constructor"""
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
+        """method with square description"""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
