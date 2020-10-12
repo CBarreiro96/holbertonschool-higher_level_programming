bf95e5e8dd2bcb025fbb6ca8a763e82b3e79e7b7
Camilo Barreiro <1845@holbertonschool.com>
2020-10-12 17:30:39 +0000

    add method create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,4 +39,16 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ returns the list of the JSON string representation json_string """
+        if not json_string or len(json_string) == 0:
+            return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
