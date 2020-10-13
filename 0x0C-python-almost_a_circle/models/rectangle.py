135870301fb3bb611477d075e44230ed27a2c891
Camilo Barreiro <1845@holbertonschool.com>
2020-10-13 17:24:49 +0000

    exercise

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module Rectangle"""
+"""
+Module Rectangle
+"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
     """Class Rectangle"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor"""
         self.width = width
