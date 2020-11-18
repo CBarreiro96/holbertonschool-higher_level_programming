87480004de4403e02da389fc33cb67f81f84bc95
Camilo Barreiro <1845@holbertonschool.com>
2020-11-18 01:46:30 +0000

    correction output 13 and 16

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,8 +3,9 @@
 -- Don't display a genre that doesn't have any shows linked
 -- Results must be sorted descending order by number of shows linked
 -- Can only use one SELECT statement
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+SELECT gr.name AS genre,
+       COUNT(shgr.genre_id) AS number_of_shows FROM tv_genres AS gr
+       JOIN tv_show_genres AS shgr
+       ON gr.id=shgr.genre_id
+       GROUP BY shgr.genre_id
+       ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,8 +3,9 @@
 -- Each record should display tv_shows.title, tv_genres.name
 -- Results must be sorted in ascending order by show title
 -- You can only use one SELECT statement
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+SELECT sh.title, gr.name FROM tv_shows AS sh
+       LEFT JOIN tv_show_genres AS shgr
+       ON sh.id=shgr.show_id
+       LEFT JOIN tv_genres AS gr
+       ON shgr.genre_id=gr.id
+       ORDER BY sh.title, gr.name ASC;
