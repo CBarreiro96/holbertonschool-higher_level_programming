51f191df2c8c14cd71569f7673fed0c80ce55e2c
Camilo Barreiro <1845@holbertonschool.com>
2020-12-12 23:04:33 +0000

    sql insertion

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-# List all states with a name starting with uppercase N
-# Username, password, and database names are given as user args
+
+'''
+List all states where 'name' matches the argument
+But this time, one safe from MySQL injection.
+Username, password, database name, and state name given as user args
+'''
+
+
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
@@ -12,15 +19,14 @@ if __name__ == "__main__":
                          port=3306)
     cur = db.cursor()
     cmd = """SELECT id, name
-             FROM states
-             WHERE name LIKE 'N%'
-             ORDER BY id ASC;"""
-    cur.execute(cmd)
+         FROM states
+         WHERE name=%s
+         ORDER BY id ASC"""
+    cur.execute(cmd, (sys.argv[4],))
     nStates = cur.fetchall()
 
     for state in nStates:
-        if (state[1][0] == 'N'):
-            print(state)
+        print(state)
 
     cur.close()
     db.close()
