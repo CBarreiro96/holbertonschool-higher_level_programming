b6b897db3edcf2d0eee57d8267d32e4c3d8bdb15
Camilo Barreiro <1845@holbertonschool.com>
2021-01-12 01:12:52 +0000

    pep8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,12 +6,13 @@ sends a POST request to the passed URL with the email
 as a parameter, and finally displays the body of the response.
 '''
 
-from  sys import argv
+
+import sys
 import requests
 
 
 if __name__ == '__main__':
-    url = argv[1]
-    payload = {'email' : argv[2]}
-    r = requests.post(url, data = payload)
+    url = sys.argv[1]
+    payload = {'email': sys.argv[2]}
+    r = requests.post(url, data=payload)
     print(r.text)
