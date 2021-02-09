c4d793920c7c73e480efc8919fe697e97074b8bc
Camilo Barreiro <1845@holbertonschool.com>
2021-02-08 19:05:09 -0500

    Exercvise

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,7 +3,7 @@
   Whit the function require include modules
   that exist in separate files (4-rectangle)
   ========= Class Square ==================
-  * Class square extend with th rectangle
+  * Class square inherits from Class rectangle
 */
 const Rectangle = require('./4-rectangle');
 
