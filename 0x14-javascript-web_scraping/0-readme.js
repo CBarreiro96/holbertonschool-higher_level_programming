1e42658566258491518817452a5822373904003e
Camilo Barreiro <1845@holbertonschool.com>
2021-02-09 12:37:44 -0500

    Add exercise readme file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+==================== README =========================
+To  interact with your computer and read file, you
+need to use require and the method readFile.
+In this case the exercise is about reads and prints
+the content of a file.
+
+*/
+const argv = process.argv;
+const fs = require('fs');
+const fileName = argv[2];
+fs.readFile(fileName, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  process.stdout.write(data);
+});
