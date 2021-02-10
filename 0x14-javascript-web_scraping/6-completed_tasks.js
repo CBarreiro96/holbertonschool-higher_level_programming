5f4b43ffb36bd04f9be9d74f86148ad6b18bd6dc
Camilo Barreiro <1845@holbertonschool.com>
2021-02-09 20:08:03 -0500

    correction output

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
+/*
+================= How many completed?  =====================
+script that computes the number of tasks completed by user id.
+*/
 const argv = process.argv;
 const url = argv[2];
-const request = require('request');
+const request = require('request');// module request
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
